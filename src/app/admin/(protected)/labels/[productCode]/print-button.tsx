2f08b912bd3b924666550d5543d77ebb3203f872
trove market <remziemiraltunbas@gmail.com>
2026-08-29 07:23:23 +0300

    Add label print control

---
+++ src/app/admin/(protected)/labels/[productCode]/print-button.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function PrintButton() {
+  return (
+    <button className="adminButton" type="button" onClick={() => window.print()}>
+      Etiketi yazdır
+    </button>
+  );
+}
